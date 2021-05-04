bcf1998db30df2e5254c6f2b6c3e45bbbedec882
Laercio Santos <laerciosantosn@gmail.com>
2021-05-04 20:06:15 -0300

    add bootstrap and created styles.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello every people</h1>
+      <h1 className="text-primary">Hello every people</h1>
     </div>
   );
 }
