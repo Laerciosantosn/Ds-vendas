aaeb9a59a938ca0dd1fb64ecc87bd64733741ad6
Laercio Santos <laerciosantosn@gmail.com>
2021-05-04 20:23:48 -0300

    created components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import Navbar from 'components/Navbar';
 import React from 'react';
 
 function App() {
   return (
     <div>
-      <h1 className="text-primary">Hello every people</h1>
+      <Navbar/>
+      <div  className="container" >
+        <h1 className="text-primary">Hello every people</h1>
+        <DataTable />
+      </div>
+      <Footer />
     </div>
   );
 }
