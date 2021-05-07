8d6b40d00f9e06ad4b3af4ee2992382984a8eb7c
Laercio Santos <laerciosantosn@gmail.com>
2021-05-07 11:33:48 -0300

    Created Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,8 @@
-import BarChart from 'components/BarChart';
-import DataTable from 'components/DataTable';
-import DonutChart from 'components/DonutChart';
-import Footer from 'components/Footer';
-import Navbar from 'components/Navbar';
-import React from 'react';
+import Routes from "Routes/index";
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <div  className="container" >
-        <h1 className="text-primary py-3">Dashboard of sales</h1>
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Tax of success</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">All sales</h5>
-            <DonutChart />
-          </div>
-        </div>
-
-        <div className="py-3">
-          <h2 className="text-primary">All Sales</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </div>
+    <Routes />
   );
 }
 
