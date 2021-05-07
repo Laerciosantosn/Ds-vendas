c7a220a03747667686be8bd5a7314b5fdd1cefe4
Laercio Santos <laerciosantosn@gmail.com>
2021-05-07 20:00:49 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { SalePage } from 'types/sale';
+import { formatCurrency, formatLocalDate } from 'utils/format';
+import { BASE_URL } from 'utils/request';
 
 
 const DataTable: React.FC = () => {
+  const [page, setPage] = useState<SalePage>({
+    first: true,
+    last: true,
+    number: 0,
+    totalElements: 0,
+    totalPages: 0,
+  });
+
+
+  useEffect(() => {
+   axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+   .then((response => {
+    setPage(response.data);
+   }))
+   
+  }, [])
+
   return (
     <div className="table-responsive">
       <table className="table table-striped table-sm">
@@ -15,55 +36,17 @@ const DataTable: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
+          {page.content?.map(sale => {
+            return (
+              <tr key={sale.id}>
+                <td>{formatLocalDate(sale.date, "dd/MM/yyyy")}</td>
+                <td>{sale.seller.name}</td>
+                <td>{sale.visited}</td>
+                <td>{sale.deals}</td>
+                <td>{formatCurrency(sale.amount)}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </div>
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,9 +4,10 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
-        <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Nelio Alves</a></p>
-        <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+        <p className="text-light">App developed by <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Nelio Alves</a></p>
+        <p className="text-light">App coded by <a href="https://github.com/laerciosantosn" target="_blank" rel="noreferrer" className="text-primary">Laércio Santos</a></p>
+        <p className="text-light"><small><strong>Week Spring React</strong><br />
+        Event promoted by DevSuperior school: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
       </div>
     </footer>
   );
