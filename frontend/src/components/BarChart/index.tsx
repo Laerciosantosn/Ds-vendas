524a2135fdf59960ec4d6f04a628772730b49d13
Laercio Santos <laerciosantosn@gmail.com>
2021-05-07 19:15:14 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/request';
+
+type SeriesData = {
+  name: string;
+  data: Array<number>;
+}
+
+type ChartData = {
+  labels: {
+    categories: Array<string>;
+  };
+  series: Array<SeriesData>;
+}
 
 const BarChart: React.FC = () => {
+
+  const [charData, setCharData] = useState<ChartData>({
+    labels: {
+      categories: []
+    },
+    series: [
+      {
+        name: "",
+        data: []
+      }
+    ]
+  })
+
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`)
+      .then((response) => {
+        const data = response.data as SaleSuccess[];
+        const mySeries = data.map(serie => round((100 * serie.deals / serie.visited), 1));
+        const myLabels = data.map(seller => seller.sellerName);
+        console.log(response)
+        setCharData({
+          labels: {
+            categories: myLabels
+          },
+          series: [
+            {
+              name: "% Success",
+              data: mySeries
+            }
+          ]
+        });
+      })
+
+  }, [])
+
+
   const options = {
     plotOptions: {
       bar: {
@@ -10,22 +63,22 @@ const BarChart: React.FC = () => {
     },
   };
 
-  const mockData = {
-    labels: {
-      categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    },
-    series: [
-      {
-        name: "% Sucesso",
-        data: [43.6, 67.1, 67.7, 45.6, 71.1]
-      }
-    ]
-  };
+  // const mockData = {
+  //   labels: {
+  //     categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+  //   },
+  //   series: [
+  //     {
+  //       name: "% Sucesso",
+  //       data: [43.6, 67.1, 67.7, 45.6, 71.1]
+  //     }
+  //   ]
+  // };
 
   return (
-    <Chart 
-      options={{...options, xaxis: mockData.labels}}
-      series={mockData.series}
+    <Chart
+      options={{ ...options, xaxis: charData.labels }}
+      series={charData.series}
       type="bar"
       height="240"
     />
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -25,10 +25,10 @@ const DonutChart: React.FC = () => {
    
     }, [])
 
-  const mockData = {
-    series: [477138, 499928, 444867, 220426, 473088],
-    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  }
+  // const mockData = {
+  //   series: [477138, 499928, 444867, 220426, 473088],
+  //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+  // }
 
   const options = {
     legend: {
